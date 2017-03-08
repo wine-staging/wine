87638c5cc0a982a2e6d3e0785a63fbe19b646d86
Michael Müller <michael@fds-team.de>
2017-03-08 17:44:17 +0100

    ntdll: Set object type for System(Extended)HandleInformation in NtQuerySystemInformation.

--- dlls/ntdll/nt.c
+++ dlls/ntdll/nt.c
@@ -2343,7 +2343,8 @@ NTSTATUS WINAPI NtQuerySystemInformation(
                         shi->Handle[i].OwnerPid     = info[i].owner;
                         shi->Handle[i].HandleValue  = info[i].handle;
                         shi->Handle[i].AccessMask   = info[i].access;
-                        /* FIXME: Fill out ObjectType, HandleFlags, ObjectPointer */
+                        shi->Handle[i].ObjectType   = info[i].type;
+                        /* FIXME: Fill out HandleFlags, ObjectPointer */
                     }
                 }
                 else if (ret == STATUS_BUFFER_TOO_SMALL)
@@ -2394,6 +2395,7 @@ NTSTATUS WINAPI NtQuerySystemInformation(
                         shi->Handle[i].UniqueProcessId = info[i].owner;
                         shi->Handle[i].HandleValue     = info[i].handle;
                         shi->Handle[i].GrantedAccess   = info[i].access;
+                        shi->Handle[i].ObjectTypeIndex = info[i].type;
                         /* FIXME: Fill out remaining fields */
                     }
                 }
