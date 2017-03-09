cbfd54641cfd3da0a56258f66829e67a92f44e1a
Michael Müller <michael@fds-team.de>
2017-03-09 16:32:59 +0100

    ntdll: Set process start time.

--- dlls/ntdll/nt.c
+++ dlls/ntdll/nt.c
@@ -2099,6 +2099,7 @@ NTSTATUS WINAPI NtQuerySystemInformation(
                             spi->UniqueProcessId = UlongToHandle(reply->pid);
                             spi->ParentProcessId = UlongToHandle(reply->ppid);
                             spi->HandleCount = reply->handles;
+                            spi->CreationTime.QuadPart = reply->start_time;
 
                             /* spi->ti will be set later on */
 
