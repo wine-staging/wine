89e583fc14345c483fb8121e4cb52842aa18b613
Michael Müller <michael@fds-team.de>
2017-04-03 05:30:27 +0200

    ntdll: Implement HashLinks field in LDR module data.

--- include/winternl.h
+++ include/winternl.h
@@ -2199,8 +2199,7 @@ typedef struct _LDR_MODULE
     ULONG               Flags;
     SHORT               LoadCount;
     SHORT               TlsIndex;
-    HANDLE              SectionHandle;
-    ULONG               CheckSum;
+    LIST_ENTRY          HashLinks;
     ULONG               TimeDateStamp;
     HANDLE              ActivationContext;
     PVOID               PatchInformation;
@@ -2210,6 +2209,9 @@ typedef struct _LDR_MODULE
     PVOID               ContextInformation;
     ULONG_PTR           OriginalBase;
     LARGE_INTEGER       LoadTime;
+
+    /* Not part of Win7 but used by Wine */
+    HANDLE              SectionHandle;
 } LDR_MODULE, *PLDR_MODULE;
 
 typedef struct _LDR_DLL_LOADED_NOTIFICATION_DATA
