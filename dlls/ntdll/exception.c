30142815ba10e4ca5145f5e727fdc4fbb6f8fe22
Andrew Wesie <awesie@gmail.com>
2018-07-27 01:26:56 -0500

    Use NtContinue to continue execution after exceptions.
    NtContinue is exported by ntdll and some programs expect it to be used to
    restore the thread context after an exception handler returns
    EXCEPTION_CONTINUE_EXECUTION. It must take a context as its first argument
    and restore that context.
    
    Current implementation is a simple wrapper around NtSetContextThread. It is
    unlikely that it has the correct behavior if called by an external caller.
    
    The __syscall_NtContinue wrapper is used so that it can be hooked by a
    third-party.

--- dlls/ntdll/exception.c
+++ dlls/ntdll/exception.c
@@ -332,8 +332,13 @@ LONG WINAPI call_unhandled_exception_filter( PEXCEPTION_POINTERS eptr )
  */
 NTSTATUS WINAPI NtContinue( CONTEXT *context, BOOLEAN alert )
 {
-    FIXME( "(%p, %d) stub!\n", context, alert );
-    return STATUS_NOT_IMPLEMENTED;
+    TRACE( "(%p, %d) stub!\n", context, alert );
+
+    /* NtSetContextThread will not have the intended behavior for a partial context. */
+    if ((context->ContextFlags & CONTEXT_FULL) != CONTEXT_FULL)
+        return STATUS_NOT_IMPLEMENTED;
+
+    return NtSetContextThread( GetCurrentThread(), context );
 }
 
 
